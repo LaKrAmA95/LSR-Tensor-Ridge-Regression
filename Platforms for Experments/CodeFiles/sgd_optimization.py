8ce3b23d63f2ab468fa225422f242bee3bb79300
Lakshitha <lakshitharamanayake@eng.pdn.ac.lk>
2024-05-25 20:22:09 -0400

    The experimentation platform for LSR_TRR with Batch and Stochastic Setting.

---
+++ Platforms for Experments/CodeFiles/sgd_optimization.py
@@ -0,0 +1,159 @@
+## Import Optimization Toolkits
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from sklearn.metrics import r2_score
+from collections import defaultdict
+from torch.utils.data import TensorDataset, DataLoader
+
+#Cost Function[Least Squares]
+#||(XW + b) - Y||_2^2
+class LeastSquares(nn.Module):
+    def __init__(self, input_dim, uses_bias = False):
+        super(LeastSquares, self).__init__() #Initialize class
+        self.linear = nn.Linear(input_dim, 1, uses_bias) #input_dim = dimension of each sample in X, output_dim = 1 = number of y values for each sample
+                
+    #Evaluate the Cost Function given X and y
+    def evaluate(self, X, Y, reduction = 'sum'):
+        mse_loss = nn.MSELoss(reduction = reduction)
+        return mse_loss(self.linear(X), Y.rehshape(-1,1))
+
+#Cost Function[Least Squares + L2 Regularization Term]
+#||(XW + b) - Y ||_2^2 + lambda * ||w||^2_2
+class RidgeRegression(nn.Module):
+    def __init__(self, input_dim, lmbd, uses_bias = False):
+        super(RidgeRegression, self).__init__()
+        self.linear = nn.Linear(input_dim, 1, uses_bias) #input_dim = dimension of each sample in X, output_dim = 1 = number of y values for each sample
+        self.lmbd = lmbd #Ridge Regression Lambda Value
+        
+    #Evaluate the Cost Function given X and y
+    def evaluate(self, X, Y, reduction = 'sum'):
+        mse_loss = nn.MSELoss(reduction = reduction)
+        return mse_loss(self.linear(X), Y) + self.l2_regularization()
+            
+    #Calculate value of lambda * ||w||^2_2
+    def l2_regularization(self):
+        return self.lmbd * (torch.norm(self.linear.weight) ** 2)
+    
+#Optimize a Cost Function via Stochastic Gradient Descent
+#X: Shape n x d where n is the number of samples and d is the number of features
+#Y: Shape n x 1 where n is the number of samples
+#cost_function_code: 0 for Normal Least Squares, 1 for Ridge Regression
+#hypers: hyperparameters
+#optimizer_code: 0 for SGD, 1 for Adagrad, 2 for RMSProp
+#p_star: estimated optimal value
+#W_true: true weights
+def SGD(X: np.ndarray, Y: np.ndarray, cost_function_code = 1, hypers = {}, optimizer_code = 0, p_star = 0, W_true = None):
+    hypers = defaultdict(int, hypers) #Convert hypers to defaultdict
+
+    #Get necessary hyperparameters
+    uses_bias = hypers['bias'] #determine whether the bias term is needed
+    lmbd = hypers['lambda'] #Lambda for ridge regression
+    lr = hypers['lr'] #learning rate
+    epochs = hypers['epochs'] #number of epochs
+    batch_size = hypers['batch_size'] #batch size to use for SGD
+
+    #Get additional hyperparameters
+    momentum = hypers['momentum']
+    dampening = hypers['dampening']
+    nesterov = hypers['nesterov']
+    decay_factor = hypers['decay_factor']
+    
+    #Initialize Cost Function
+    if cost_function_code == 0:
+        cost_function = LeastSquares(X.shape[1], uses_bias)
+    elif cost_function_code == 1:
+        cost_function = RidgeRegression(X.shape[1], lmbd, uses_bias)
+    
+    #Convert X and Y to pytorch tensors
+    X = torch.tensor(X, dtype = torch.float32)
+    Y = torch.tensor(Y, dtype = torch.float32)
+    
+    #Splitting data into minibatches 
+    dataset = TensorDataset(X,Y)
+    dataloader = DataLoader(dataset, batch_size = batch_size,  shuffle=True )
+    
+    #Initialize Optimizer
+    if optimizer_code == 0:
+        optimizer = optim.SGD(cost_function.parameters(), lr = lr, momentum = momentum, dampening = dampening, nesterov = nesterov)
+    elif optimizer_code == 1:
+        optimizer = optim.Adagrad(cost_function.parameters(), lr = lr)
+    elif optimizer_code == 2:
+        optimizer = optim.RMSprop(cost_function.parameters(), lr = lr, alpha = decay_factor, momentum = momentum)
+    
+    #Store batch loss values
+    loss_values = []
+    
+    #Store gap to optimality
+    gap_to_optimality = []
+    
+    #Store Metric Values
+
+    nmse_values = []
+    corr_values = []
+    R2_values = []
+
+    #Training Loop
+    for epoch in range(epochs):
+
+        for X_sample, Y_sample in dataloader:
+        
+            # Shuffle dataset
+            #indices = torch.randperm(X.size(0))
+            #X_shuffled = X[indices]
+            #y_shuffled = Y[indices]
+        
+            #Get X and Y sample
+            #X_sample = X_shuffled[0: batch_size]
+            #Y_sample = y_shuffled[0: batch_size]
+            
+            Y_sample = Y_sample.reshape(-1,1)
+
+            
+            # Compute stochastic loss
+            stochastic_loss = cost_function.evaluate(X_sample, Y_sample, 'sum')
+
+            # Zero gradients
+            optimizer.zero_grad()
+
+            # Backward pass to compute stochastic gradient
+            stochastic_loss.backward()
+
+            # Update parameters
+            optimizer.step()
+
+        
+            
+        #print('------------------------------------------------------------------epoch:',epoch,'completed------------------------------------------------------------------')        
+        #Print and Store batch loss values
+        batch_loss = cost_function.evaluate(X, Y, 'sum')
+        loss_values.append(batch_loss.item())
+        gap_to_optimality.append(batch_loss.item() - p_star)
+        
+        #Calculate Metrics
+        weights = cost_function.linear.weight.data.numpy().reshape((-1, 1))
+        bias = cost_function.linear.bias.item() if uses_bias else 0
+        X_numpy = X.numpy()
+        Y_predicted = X_numpy @ weights + bias
+        Y_numpy = Y.numpy()
+        
+
+        nmse = np.sum(np.square((Y_predicted - Y_numpy))) / np.sum(np.square(Y_numpy))
+        correlation = np.corrcoef(Y_predicted.flatten(), Y_numpy.flatten())[0, 1]
+        R2_score = r2_score(Y_numpy, Y_predicted)
+        
+        nmse_values.append(nmse)
+        corr_values.append(correlation)
+        R2_values.append(R2_score)
+        
+        
+        #print(f'Epoch [{epoch+1}/{epochs}], Loss: {batch_loss:.4f}, Gap to Optimality: {gap_to_optimality[-1]:.4f}, NMSE: {nmse}, Correlation: {correlation}, R2: {R2_score}')
+    
+    weights = cost_function.linear.weight.data.numpy().reshape((-1, 1)) #Return weights as numpy array
+
+    #return weights and bias and loss metrics
+    if uses_bias:
+        return weights, cost_function.linear.bias.item(), loss_values, gap_to_optimality, nmse_values, corr_values, R2_values
+    else:
+        return weights, 0, loss_values, gap_to_optimality, nmse_values, corr_values, R2_values
---
+++ Platforms for Experments/CodeFiles/train_test_closed.py
@@ -0,0 +1,87 @@
+from sklearn.linear_model import Ridge
+import numpy as np
+from LSR_Tensor_2D_v1 import LSR_tensor_dot
+from lsr_bcd_regression_closed import lsr_bcd_regression
+from optimization_closed import inner_product, R2, objective_function_vectorized
+
+def train_test(X_train: np.ndarray, Y_train: np.ndarray, X_test: np.ndarray, Y_test: np.ndarray, B_tensored: np.ndarray, lambda1, hypers,Y_train_mean, intercept = False):
+  hypers['weight_decay'] = lambda1
+
+  #Define LSR Tensor Hyperparameters
+  ranks = hypers['ranks']
+  separation_rank = hypers['separation_rank']
+  LSR_tensor_dot_shape = tuple(X_train.shape)[1:]
+  need_intercept = intercept
+
+  #Construct LSR Tensor
+  lsr_tensor = LSR_tensor_dot(shape = LSR_tensor_dot_shape, ranks = ranks, separation_rank = separation_rank, intercept = need_intercept)
+  lsr_tensor, objective_function_values = lsr_bcd_regression(lsr_tensor, X_train, Y_train, hypers,intercept = need_intercept)
+  expanded_lsr = lsr_tensor.expand_to_tensor()
+  expanded_lsr = np.reshape(expanded_lsr, X_train[0].shape, order = 'F')
+
+  Y_test_predicted = inner_product(np.transpose(X_test, (0, 2, 1)), expanded_lsr.flatten(order ='F')) + lsr_tensor.b + Y_train_mean
+
+
+  print(f"Y_test_predicted: {Y_test_predicted.flatten()}, Y_test: {Y_test.flatten()}")
+  test_nmse_loss = np.sum(np.square((Y_test_predicted.flatten() - Y_test.flatten()))) / np.sum(np.square(Y_test.flatten()))
+  normalized_estimation_error = ((np.linalg.norm(expanded_lsr - B_tensored)) ** 2) /  ((np.linalg.norm(B_tensored)) ** 2)
+  test_R2_loss = R2(Y_test.flatten(), Y_test_predicted.flatten())
+  test_correlation = np.corrcoef(Y_test_predicted.flatten(), Y_test.flatten())[0, 1]
+
+  print("Y Test Predicted: ", Y_test_predicted.flatten())
+  print("Y Test Actual: ", Y_test.flatten())
+
+  return normalized_estimation_error, test_nmse_loss, test_R2_loss, test_correlation, objective_function_values
+
+#Given the Best Lambda found by KFoldCV, train a LRR model with that best lambda and generate test metrics!
+def TrainTest_Vectorized(X_train, Y_train, X_test, Y_test, lambda1,B_tensored = None, intercept = False):
+    
+    # need intercept
+
+    need_intercept = intercept 
+    
+    #Flatten B_tensored
+    if B_tensored is not None:
+      B_true = B_tensored.flatten()
+
+    #Flatten Y_train and Y_test
+    Y_train = Y_train.flatten()
+    Y_test = Y_test.flatten()
+
+    #Fit LRR Model on X_train, Y_train
+    lrr_model = Ridge(alpha = lambda1, solver = 'svd', fit_intercept = need_intercept)
+    lrr_model.fit(X_train, Y_train)
+    w_flattened = lrr_model.coef_.flatten()
+
+    #intercept
+    if need_intercept:
+      b = lrr_model.intercept_
+      print('Itercept and Test:',b)
+
+    #having objective function value
+    p_star = objective_function_vectorized(Y_train, X_train, w_flattened.reshape((-1, 1)), lambda1, b if need_intercept else None)
+
+
+    #Using the fitted LRR Model, get the Predicted Test Data
+    Y_test_predicted = lrr_model.predict(X_test).flatten()
+
+    #Compute NEE, NMSE, Correlation, and R^2 Score
+    if B_tensored is not None: 
+      test_normalized_estimation_error = ((np.linalg.norm(w_flattened - B_true)) ** 2) /  ((np.linalg.norm(B_true)) ** 2)
+    test_nmse_loss = np.sum(np.square((Y_test_predicted - Y_test))) / np.sum(np.square(Y_test))
+    test_correlation = np.corrcoef(Y_test_predicted, Y_test)[0, 1]
+    test_R2_score = lrr_model.score(X_test, Y_test)
+
+    #Print Test Results
+    if B_tensored is not None: 
+      print(f"NEE: {test_normalized_estimation_error}, NMSE: {test_nmse_loss}, Correlation: {test_correlation}, R^2 Score: {test_R2_score}, p_stat: {p_star}")
+    else:
+      print(f"NMSE: {test_nmse_loss}, Correlation: {test_correlation}, R^2 Score: {test_R2_score}, p_stat: {p_star}")
+    
+    
+    #Return Test Results
+    if B_tensored is not None:
+      return test_normalized_estimation_error, test_nmse_loss, test_correlation, test_R2_score,Y_test_predicted,p_star
+    else:
+      test_normalized_estimation_error = float('inf')
+      return test_normalized_estimation_error,test_nmse_loss, test_correlation, test_R2_score,Y_test_predicted,p_star
