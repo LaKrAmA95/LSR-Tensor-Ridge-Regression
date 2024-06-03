109bffccee656e40bb1207e9ce3007d2e4184467
Ravi Raghavan <ravi0308raghavan@gmail.com>
2024-06-02 22:13:54 -0400

    Working on Plots for SGD

--- Vector Regression Baseline/Code Files/optimization.py
+++ Vector Regression Baseline/Code Files/optimization.py
@@ -10,9 +10,15 @@ from torch.utils.data import TensorDataset, DataLoader
 #Cost Function[Least Squares]
 #||(XW + b) - Y||_2^2
 class LeastSquares(nn.Module):
-    def __init__(self, input_dim, uses_bias = False):
+    def __init__(self, input_dim, uses_bias = False, W_init = None):
         super(LeastSquares, self).__init__() #Initialize class
         self.linear = nn.Linear(input_dim, 1, uses_bias) #input_dim = dimension of each sample in X, output_dim = 1 = number of y values for each sample
+        
+        if isinstance(W_init, np.ndarray):
+            W_init = W_init.reshape(1, input_dim)
+            tensor = torch.from_numpy(W_init).float()
+            with torch.no_grad():
+                self.linear.weight.copy_(tensor)
                 
     #Evaluate the Cost Function given X and y
     def evaluate(self, X, Y, reduction = 'sum'):
@@ -22,10 +28,16 @@ class LeastSquares(nn.Module):
 #Cost Function[Least Squares + L2 Regularization Term]
 #||(XW + b) - Y ||_2^2 + lambda * ||w||^2_2
 class RidgeRegression(nn.Module):
-    def __init__(self, input_dim, lmbd, uses_bias = False):
+    def __init__(self, input_dim, lmbd, uses_bias = False, W_init = None):
         super(RidgeRegression, self).__init__()
         self.linear = nn.Linear(input_dim, 1, uses_bias) #input_dim = dimension of each sample in X, output_dim = 1 = number of y values for each sample
         self.lmbd = lmbd #Ridge Regression Lambda Value
+        
+        if isinstance(W_init, np.ndarray):
+            W_init = W_init.reshape(1, input_dim)
+            tensor = torch.from_numpy(W_init).float()
+            with torch.no_grad():
+                self.linear.weight.copy_(tensor)
         
     #Evaluate the Cost Function given X and y
     def evaluate(self, X, Y, reduction = 'sum'):
@@ -62,7 +74,7 @@ def exact_line_search_RR(X: np.ndarray, Y: np.ndarray, lmbd, cost_function, uses
 #optimizer_code: 0 for SGD, 1 for Adagrad, 2 for RMSProp
 #p_star: estimated optimal value
 #W_true: true weights
-def SGD(X: np.ndarray, Y: np.ndarray, cost_function_code = 1, hypers = {}, optimizer_code = 0, p_star = 0, W_true = None):
+def SGD(X: np.ndarray, Y: np.ndarray, cost_function_code = 1, hypers = {}, optimizer_code = 0, p_star = 0, W_true = None, W_init = None):
     hypers = defaultdict(int, hypers) #Convert hypers to defaultdict
     
     #Get necessary hyperparameters
@@ -80,9 +92,9 @@ def SGD(X: np.ndarray, Y: np.ndarray, cost_function_code = 1, hypers = {}, optim
     
     #Initialize Cost Function
     if cost_function_code == 0:
-        cost_function = LeastSquares(X.shape[1], uses_bias)
+        cost_function = LeastSquares(X.shape[1], uses_bias, W_init)
     elif cost_function_code == 1:
-        cost_function = RidgeRegression(X.shape[1], lmbd, uses_bias)
+        cost_function = RidgeRegression(X.shape[1], lmbd, uses_bias, W_init)
     
     #Convert X and Y to pytorch tensors
     X_tensor = torch.tensor(X, dtype = torch.float32)
@@ -112,14 +124,83 @@ def SGD(X: np.ndarray, Y: np.ndarray, cost_function_code = 1, hypers = {}, optim
     #Store gap to optimality
     gap_to_optimality = []
     
-    #Store Metric Values 
+    #Store NEE, NMSE, Correlation, and R2 Metrics
     nee_values = []
     nmse_values = []
     corr_values = []
     R2_values = []
+    
+    #Store Fixed Point Stochastic Gradient Norms
+    fixed_point_stochastic_gradients = []
+    fixed_point_stochastic_gradient_norms = []
+    
+    #Store Fixed Point Full Gradient Norms
+    fixed_point_full_gradients = []
+    fixed_point_full_gradient_norms = []
+    
+    #Store Iterate Norms
+    iterates = []
+    iterate_norms = []
+    
+    #Store Full Gradient Norms
+    full_gradients = []
+    full_gradient_norms = []
+    
+    #Store Stochastic Gradient Norms
+    stochastic_gradients = []
+    stochastic_gradient_norms = []
+        
+    #Keep track of number of epochs ran
+    epochs_ran = 0
 
     #Training Loop
     for epoch in range(epochs):
+        epochs_ran += 1
+        
+        #Analysis to Compute Fixed Point Full Gradient Norms
+        #Compute Full Loss
+        loss = cost_function.evaluate(X_tensor, Y_tensor, 'sum')
+        
+        # Zero gradients
+        optimizer.zero_grad()
+
+        # Backward pass to compute full gradient
+        loss.backward()
+        
+        #Store Fixed Point Full Gradient Norm
+        fixed_point_full_gradients.append(cost_function.linear.weight.grad)
+        fixed_point_full_gradient_norms.append(torch.norm(cost_function.linear.weight.grad))
+        
+        #Analysis to compute Fixed Point Stochastic Gradient norms
+        for X_sample, Y_sample in dataloader:
+            # Compute stochastic loss
+            stochastic_loss = cost_function.evaluate(X_sample, Y_sample, 'sum')
+
+            # Zero gradients
+            optimizer.zero_grad()
+
+            # Backward pass to compute stochastic gradient
+            stochastic_loss.backward()
+            
+            #Store Fixed Point Gradient Norm
+            fixed_point_stochastic_gradients.append(cost_function.linear.weight.grad)
+            fixed_point_stochastic_gradient_norms.append(torch.norm(cost_function.linear.weight.grad))
+            
+        #Store Entire Gradient Norm
+        ## Compute loss
+        loss = cost_function.evaluate(X_tensor, Y_tensor, 'sum')
+        
+        ## Zero gradients
+        optimizer.zero_grad()
+
+        ## Backward pass to compute gradient
+        loss.backward()
+        
+        ## Store Gradient Norm
+        full_gradients.append(cost_function.linear.weight.grad)
+        full_gradient_norms.append(torch.norm(cost_function.linear.weight.grad))
+        
+        #Perform Stochastic Updates
         for X_sample, Y_sample in dataloader:        
             # Compute stochastic loss
             stochastic_loss = cost_function.evaluate(X_sample, Y_sample, 'sum')
@@ -129,161 +210,73 @@ def SGD(X: np.ndarray, Y: np.ndarray, cost_function_code = 1, hypers = {}, optim
 
             # Backward pass to compute stochastic gradient
             stochastic_loss.backward()
+            
+            #Store Stochastic Gradient Norm
+            stochastic_gradients.append(cost_function.linear.weight.grad)
+            stochastic_gradient_norms.append(torch.norm(cost_function.linear.weight.grad))
 
             # Update parameters
             optimizer.step()
             
-        #Print and Store batch loss values
-        batch_loss = cost_function.evaluate(X_tensor, Y_tensor, 'sum')
-        loss_values.append(batch_loss.item())
-        gap_to_optimality.append(batch_loss.item() - p_star)
-        
-        #Calculate Metrics
-        weights = cost_function.linear.weight.data.numpy().reshape((-1, 1))
-        bias = cost_function.linear.bias.item() if uses_bias else 0
-        X_numpy = X_tensor.numpy()
-        Y_predicted = X_numpy @ weights + bias
-        Y_numpy = Y_tensor.numpy()
-        
-        nee = ((np.linalg.norm(weights - W_true)) ** 2) /  ((np.linalg.norm(W_true)) ** 2)
-        nmse = np.sum(np.square((Y_predicted - Y_numpy))) / np.sum(np.square(Y_numpy))
-        correlation = np.corrcoef(Y_predicted.flatten(), Y_numpy.flatten())[0, 1]
-        R2_score = r2_score(Y_numpy, Y_predicted)
-        
-        nee_values.append(nee)
-        nmse_values.append(nmse)
-        corr_values.append(correlation)
-        R2_values.append(R2_score)
-        
-        # Print gradients
-        for name, param in cost_function.named_parameters():
-            if param.grad is not None:
-                print(f"Gradient Norm for {name}: {torch.norm(param.grad)}")
-            else:
-                print(f"No gradient Norm for {name}")
-        
-        print(f'Epoch [{epoch+1}/{epochs}], Loss: {loss_values[-1]:.4f}, Gap to Optimality: {gap_to_optimality[-1]:.4f}, NEE: {nee}, NMSE: {nmse}, Correlation: {correlation}, R2: {R2_score}')
-
-    weights = cost_function.linear.weight.data.numpy().reshape((-1, 1)) #Return weights as numpy array
-
-    #return weights and bias and loss metrics
-    if uses_bias:
-        return weights, cost_function.linear.bias.item(), loss_values, gap_to_optimality, nee_values, nmse_values, corr_values, R2_values
-    else:
-        return weights, 0, loss_values, gap_to_optimality, nee_values, nmse_values, corr_values, R2_values
-
-
-#Optimize a Cost Function via Gradient Descent with Fixed Step Size
-#X: Shape n x d where n is the number of samples and d is the number of features
-#Y: Shape n x 1 where n is the number of samples
-#cost_function_code: 0 for Normal Least Squares, 1 for Ridge Regression
-#hypers: hyperparameters
-#optimizer_code: 0 for SGD, 1 for Adagrad, 2 for RMSProp
-#p_star: estimated optimal value
-#W_true: true weights
-def GD1(X: np.ndarray, Y: np.ndarray, cost_function_code = 1, hypers = {}, optimizer_code = 0, p_star = 0, W_true = None):
-    hypers = defaultdict(int, hypers) #Convert hypers to defaultdict
-    
-    #Get necessary hyperparameters
-    uses_bias = hypers['bias'] #determine whether the bias term is needed
-    lmbd = hypers['lambda'] #Lambda for ridge regression
-    lr = hypers['lr'] #learning rate
-    epochs = hypers['epochs'] #number of epochs
-    
-    #Get additional hyperparameters
-    momentum = hypers['momentum']
-    dampening = hypers['dampening']
-    nesterov = hypers['nesterov']
-    decay_factor = hypers['decay_factor']
-    
-    #Initialize Cost Function
-    if cost_function_code == 0:
-        cost_function = LeastSquares(X.shape[1], uses_bias)
-    elif cost_function_code == 1:
-        cost_function = RidgeRegression(X.shape[1], lmbd, uses_bias)
-    
-    #Convert X and Y to pytorch tensors
-    X_tensor = torch.tensor(X, dtype = torch.float32)
-    Y_tensor = torch.tensor(Y, dtype = torch.float32)
-    
-    #If W_true is None, set it to a zero vector
-    if not isinstance(W_true, np.ndarray):
-        W_true = np.zeros(shape = (X.shape[1], 1))
-    
-    #Initialize Optimizer
-    if optimizer_code == 0:
-        optimizer = optim.SGD(cost_function.parameters(), lr = lr, momentum = momentum, dampening = dampening, nesterov = nesterov)
-    elif optimizer_code == 1:
-        optimizer = optim.Adagrad(cost_function.parameters(), lr = lr)
-    elif optimizer_code == 2:
-        optimizer = optim.RMSprop(cost_function.parameters(), lr = lr, alpha = decay_factor, momentum = momentum)
-    elif optimizer_code == 3:
-        optimizer = optim.Adam(cost_function.parameters(), lr = lr)
-    
-    #Store batch loss values
-    loss_values = []
-    
-    #Store gap to optimality
-    gap_to_optimality = []
-    
-    #Store Metric Values 
-    nee_values = []
-    nmse_values = []
-    corr_values = []
-    R2_values = []
-
-    #Training Loop
-    for epoch in range(epochs):
-        # Compute loss
+            #Get Weights and Bias
+            weights = cost_function.linear.weight.data.numpy().reshape((-1, 1))
+            bias = cost_function.linear.bias.item() if uses_bias else 0
+            
+            #Store Iterate Norm
+            iterates.append(weights)
+            iterate_norms.append(np.linalg.norm(weights))
+                    
+        #Print and Store Batch Loss values
         loss = cost_function.evaluate(X_tensor, Y_tensor, 'sum')
-
-        # Zero gradients
-        optimizer.zero_grad()
-
-        # Backward pass to compute gradient
-        loss.backward()
-
-        # Update parameters
-        optimizer.step()
+        optimizer.zero_grad() #zero out gradients
+        loss.backward() #compute gradients
+        loss_values.append(loss.item())
+        gap_to_optimality.append(loss.item() - p_star)
         
-        #Print and Store loss values
-        loss_value = cost_function.evaluate(X_tensor, Y_tensor, 'sum').item()
-        loss_values.append(loss_value)
-        gap_to_optimality.append(loss_value - p_star)
-        
-        #Calculate Metrics
+        #Calculate NEE, NMSE, Correlation, and R2 Metrics
         weights = cost_function.linear.weight.data.numpy().reshape((-1, 1))
         bias = cost_function.linear.bias.item() if uses_bias else 0
         X_numpy = X_tensor.numpy()
         Y_predicted = X_numpy @ weights + bias
         Y_numpy = Y_tensor.numpy()
-        
         nee = ((np.linalg.norm(weights - W_true)) ** 2) /  ((np.linalg.norm(W_true)) ** 2)
         nmse = np.sum(np.square((Y_predicted - Y_numpy))) / np.sum(np.square(Y_numpy))
         correlation = np.corrcoef(Y_predicted.flatten(), Y_numpy.flatten())[0, 1]
         R2_score = r2_score(Y_numpy, Y_predicted)
         
+        #Store NEE, NMSE, Correlation, and R2 Metrics
         nee_values.append(nee)
         nmse_values.append(nmse)
         corr_values.append(correlation)
-        R2_values.append(R2_score)
+        R2_values.append(R2_score)                
+                
+        #Print Values during Iterations
+        print(f'Epoch [{epoch+1}/{epochs}], Loss: {loss_values[-1]:.4f}, Gap to Optimality: {gap_to_optimality[-1]:.4f}, Fixed Point Stochastic Gradient Norm: {fixed_point_stochastic_gradient_norms[-1]:.4f}, Fixed Point Full Gradient Norm: {fixed_point_full_gradient_norms[-1]:.4f}, Iterate Norm: {iterate_norms[-1]:.4f}, Full Gradient Norm: {full_gradient_norms[-1]:.4f},  Stochastic Gradient Norm: {stochastic_gradient_norms[-1]:.4f}, NEE: {nee}, NMSE: {nmse}, Correlation: {correlation}, R2: {R2_score}')
         
-        # Print gradients
-        for name, param in cost_function.named_parameters():
-            if param.grad is not None:
-                print(f"Gradient Norm for {name}: {torch.norm(param.grad)}")
-            else:
-                print(f"No gradient Norm for {name}")
+        #If Stopping Criteria has been satisifed, break
+        if torch.norm(cost_function.linear.weight.grad) <= 1:
+            break
         
-        print(f'Epoch [{epoch+1}/{epochs}], Loss: {loss_value:.4f}, Gap to Optimality: {gap_to_optimality[-1]:.4f}, NEE: {nee}, NMSE: {nmse}, Correlation: {correlation}, R2: {R2_score}')
+    #Convert everything to numpy array
+    loss_values = np.array(loss_values)
+    gap_to_optimality = np.array(gap_to_optimality)
+    nee_values = np.array(nee_values)
+    nmse_values = np.array(nmse_values)
+    corr_values = np.array(corr_values)
+    R2_values = np.array(R2_values)
+    fixed_point_stochastic_gradient_norms = np.array(fixed_point_stochastic_gradient_norms)
+    fixed_point_full_gradient_norms = np.array(fixed_point_full_gradient_norms)
+    iterate_norms = np.array(iterate_norms)
+    full_gradient_norms = np.array(full_gradient_norms)
+    stochastic_gradient_norms = np.array(stochastic_gradient_norms)
 
     weights = cost_function.linear.weight.data.numpy().reshape((-1, 1)) #Return weights as numpy array
 
     #return weights and bias and loss metrics
     if uses_bias:
-        return weights, cost_function.linear.bias.item(), loss_values, gap_to_optimality, nee_values, nmse_values, corr_values, R2_values
+        return weights, cost_function.linear.bias.item(), loss_values, gap_to_optimality, fixed_point_stochastic_gradients, fixed_point_stochastic_gradient_norms, fixed_point_full_gradients, fixed_point_full_gradient_norms, iterates, iterate_norms, full_gradients, full_gradient_norms, stochastic_gradients, stochastic_gradient_norms, nee_values, nmse_values, corr_values, R2_values, epochs_ran
     else:
-        return weights, 0, loss_values, gap_to_optimality, nee_values, nmse_values, corr_values, R2_values
+        return weights, 0, loss_values, gap_to_optimality, fixed_point_stochastic_gradients, fixed_point_stochastic_gradient_norms, fixed_point_full_gradients, fixed_point_full_gradient_norms, iterates, iterate_norms, full_gradients, full_gradient_norms, stochastic_gradients, stochastic_gradient_norms, nee_values, nmse_values, corr_values, R2_values, epochs_ran
     
 #Optimize a Cost Function via Gradient Descent with Exact Line Search
 #X: Shape n x d where n is the number of samples and d is the number of features
@@ -292,7 +285,7 @@ def GD1(X: np.ndarray, Y: np.ndarray, cost_function_code = 1, hypers = {}, optim
 #hypers: hyperparameters
 #p_star: estimated optimal value
 #W_true: true weights
-def GD2(X: np.ndarray, Y: np.ndarray, cost_function_code = 1, hypers = {}, p_star = 0, W_true = None):
+def GD(X: np.ndarray, Y: np.ndarray, cost_function_code = 1, hypers = {}, p_star = 0, W_true = None):
     hypers = defaultdict(int, hypers) #Convert hypers to defaultdict
     
     #Get necessary hyperparameters
@@ -314,7 +307,7 @@ def GD2(X: np.ndarray, Y: np.ndarray, cost_function_code = 1, hypers = {}, p_sta
     if not isinstance(W_true, np.ndarray):
         W_true = np.zeros(shape = (X.shape[1], 1))
         
-    #Store batch loss values
+    #Store loss values
     loss_values = []
     
     #Store gap to optimality
@@ -340,13 +333,12 @@ def GD2(X: np.ndarray, Y: np.ndarray, cost_function_code = 1, hypers = {}, p_sta
         loss.backward()
 
         # Update parameters
-        t = exact_line_search_RR(X, Y, lmbd, cost_function, uses_bias)
-        print(f"Value of t is: {t}")
+        lr = exact_line_search_RR(X, Y, lmbd, cost_function, uses_bias)
         
         # Manually update the weights and biases
         with torch.no_grad():
             for param in cost_function.parameters():
-                param -= t * param.grad
+                param -= lr * param.grad
         
         #Print and Store loss values
         loss_value = cost_function.evaluate(X_tensor, Y_tensor, 'sum').item()
@@ -370,7 +362,8 @@ def GD2(X: np.ndarray, Y: np.ndarray, cost_function_code = 1, hypers = {}, p_sta
         corr_values.append(correlation)
         R2_values.append(R2_score)
                 
-        print(f'Epoch [{epoch+1}/{epochs}], Loss: {loss_value:.4f}, Gap to Optimality: {gap_to_optimality[-1]:.4f}, NEE: {nee}, NMSE: {nmse}, Correlation: {correlation}, R2: {R2_score}')
+        weight_norm = np.linalg.norm(weights)
+        print(f'Epoch [{epoch+1}/{epochs}], Learning Rate: {lr:.4f}, Loss: {loss_value:.4f}, Gap to Optimality: {gap_to_optimality[-1]:.4f}, Iterate Norm: {weight_norm}, NEE: {nee}, NMSE: {nmse}, Correlation: {correlation}, R2: {R2_score}')
         
         # Stopping Criteria
         criteria_satisfied = True
